a6fd14f32c473bb3896697eb2bf09ed4b4fa132d
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-06 15:53:06 +0530

    .

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const workspaceRoutes = require('./routes/workspaces');
+const taskRoutes = require('./routes/tasks');
+const chatRoutes = require('./routes/chat');
+const documentRoutes = require('./routes/documents');
+
+const { setupSocketHandlers } = require('./socket/socketHandlers');
+const { setupWebRTCHandlers } = require('./socket/webrtcHandlers');
+const { setupYWebSocket } = require('./yjs/yWebSocketServer');
+
 const app = express();
+const server = http.createServer(app);
+
+const defaultOrigins = [
+  'http://localhost:3000'
+];
+const envOrigins = ('http://localhost:3000' || process.env.FRONTEND_URL || '')
+  .split(',')
+  .map((s) => s.trim())
+  .filter(Boolean);
+const allowedOrigins = envOrigins.length ? envOrigins : defaultOrigins;
 
 const PORT = process.env.PORT || 5000;
